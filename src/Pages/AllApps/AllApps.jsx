683b64f71fa9e649465f4da56718637b3d7aba95
Hafizul Islam <hafizul2004official@gmail.com>
2026-06-24 00:27:31 +0600

    Filter Section Add

--- src/Pages/AllApps/AllApps.jsx
+++ src/Pages/AllApps/AllApps.jsx
@@ -1,10 +1,16 @@
 import { useLoaderData } from "react-router";
 import AllDataApp from "../AllDataApp/AllDataApp";
+import { useState } from "react";
+import AppsError from "../AppsError/AppsError";
 // import AllDataApp from "../AllDataApp/AllDataApp";
 
 const AllApps = () => {
 
     const AllData = useLoaderData()
+
+    const [search, setSearch] = useState("")
+
+    const FilterData = AllData.filter(app => app.title.toLowerCase().includes(search.toLowerCase()))
 
     console.log(AllData);
 
@@ -16,31 +22,37 @@ const AllApps = () => {
             </div>
             <div className="flex justify-between items-center mt-4 mx-8 sm:mx-10">
                 <div className="font-bold sm:text-xl lg:text-2xl">
-                    <h1>Apps Found ({AllData.length})</h1>
+                    <h1>Apps Found ({FilterData.length})</h1>
                 </div>
                 <div>
                     <label className="input">
-                    <svg className="h-[1em] opacity-" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <g
-                            strokeLinejoin="round"
-                            strokeLinecap="round"
-                            strokeWidth="2.5"
-                            fill="none"
-                            stroke="currentColor"
-                        >
-                            <circle cx="11" cy="11" r="8"></circle>
-                            <path d="m21 21-4.3-4.3"></path>
-                        </g>
-                    </svg>
-                    <input type="search" required placeholder="Search" />
-                </label>
+                        <svg className="h-[1em] opacity-" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <g
+                                strokeLinejoin="round"
+                                strokeLinecap="round"
+                                strokeWidth="2.5"
+                                fill="none"
+                                stroke="currentColor"
+                            >
+                                <circle cx="11" cy="11" r="8"></circle>
+                                <path d="m21 21-4.3-4.3"></path>
+                            </g>
+                        </svg>
+                        <input type="search" required placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)} />
+                    </label>
                 </div>
             </div>
 
 
             <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 pt-4 mx-8 items-center">
-                {
-                    AllData.map(Data => <AllDataApp AllBooks={Data} key={Data.id}></AllDataApp>)
+               {
+                    FilterData.length === 0 ? (
+                        <div className="col-span-full w-full">
+                            <AppsError></AppsError>
+                        </div>
+                    ) : (
+                        FilterData.map(Data => <AllDataApp AllBooks={Data} key={Data.id}></AllDataApp>)
+                    )
                 }
             </div>
         </div>
